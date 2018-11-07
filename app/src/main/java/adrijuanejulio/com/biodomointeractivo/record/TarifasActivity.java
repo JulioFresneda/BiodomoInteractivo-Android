beab692782f57a51df0f7b749408147c8613031c
Adrián Jesús Peña Rodríguez <adrian.j.p.r@gmail.com>
2018-11-07 16:18:33 +0100

    New

---
+++ app/src/main/java/adrijuanejulio/com/biodomointeractivo/record/TarifasActivity.java
@@ -0,0 +1,13 @@
+package adrijuanejulio.com.biodomointeractivo.record;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TarifasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarifas);
+    }
+}
