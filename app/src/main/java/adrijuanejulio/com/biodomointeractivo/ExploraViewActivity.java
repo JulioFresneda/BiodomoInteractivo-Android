e2a57f896da9b7c717d84723d14ccd9c0f3e7dcc
juliofgx17 <juliofgx17@gmail.com>
2018-11-10 15:48:32 +0100

    Menu rediseñado

--- app/src/main/java/adrijuanejulio/com/biodomointeractivo/ExploraViewActivity.java
+++ app/src/main/java/adrijuanejulio/com/biodomointeractivo/ExploraViewActivity.java
@@ -56,6 +56,7 @@ public class ExploraViewActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explora_view);
 
